63915ed447cddd4a49e330e5e64fa4ac4878f6d5
LinZong <linshengyi123@hotmail.com>
2024-08-18 17:03:59 +0800

    Fix crashing on saving host entry in release build

--- app/src/main/java/moe/nemesiss/hostman/model/FileOperationResult.kt
+++ app/src/main/java/moe/nemesiss/hostman/model/FileOperationResult.kt
@@ -1,10 +1,11 @@
 package moe.nemesiss.hostman.model
 
 import com.alibaba.fastjson2.JSON
+import moe.nemesiss.hostman.proguard.NoProguard
 
 data class FileOperationResult(val success: Boolean = true,
                                val message: String = "",
-                               val exceptionStack: String? = null) {
+                               val exceptionStack: String? = null) : NoProguard {
     companion object {
         fun deserialize(stringifyResult: String): FileOperationResult {
             return JSON.parseObject(stringifyResult, FileOperationResult::class.java)
